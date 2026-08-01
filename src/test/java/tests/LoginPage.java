b8d07cdf5ad75e1b904837f1530421581c4f02d8
pavithra.s01 <pavithra.s01@PAVITHRA>
2026-08-01 10:03:05 +0530

    created loginpage....

---
+++ src/test/java/tests/LoginPage.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	@Test
+	public void login() {
+		System.out.println("enter login credentials");
+	}
+	
+	@Test
+	public void loginp() {
+		System.out.println("login successful");
+	}
+
+}
